295b9086c997638dda4f75590598247f05e45592
Volodymyr Lantrat <stivenuaking@gmail.com>
2026-03-15 16:29:28 +0200

    fix

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,6 +1,3 @@
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm.js";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
 
